7871b2e1ffdcd4b270349c06edfc4bbe16891097
Michael Martinez <ceo@mi-martinez.com>
2022-08-31 23:04:31 -0500

    FEATURE: App conections from to components

--- src/App.js
+++ src/App.js
@@ -5,12 +5,28 @@
 // MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
 // PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
 
+import Cabecera from './components/Cabecera'
+import Listado from './components/Listado'
+import React from 'react'
+import { useState } from 'react'
+import './index.css'
+
+
 function App() {
+
+  const [total, setTotal] = useState(
+    {
+      stock: 0,
+      quantity: 0
+    }
+  );
+
+  
 
   return (
     <div className="App">
-      <Cabecera />
-      <Listado />
+      <Cabecera total={total} />
+     <Listado total={total} setTotal={setTotal} /> 
     </div>
   );
 }
--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
+import Item from './Item'
 
-// Debemos importar el listado de objetos del archivo JSON para usarlos como array a la hora de crear cada Item.
-// El componente Listado es el padre de:
-// - varios Item
-// ESTADO: Listado no necesita manejar un estado.
-// MÉTODOS: Listado no requiere de métodos.
-// PROPS: Listado recibe el método para aumentar el estado de App y se lo pasa a cada uno de sus hijos.
+import './data.json'
 
-export default function Listado() {
+
+export default function Listado({total, setTotal}) {
+
+  const data = require('./data.json')
+
   return (
     <div className='container'>
-      {/* renderizamos los Item aquí */}
+      {data.map(item => (
+        <Item key={item.id} item={item} total={total} setTotal={setTotal} />
+      ))}
+      
     </div>
   )
 }
